61dded40edd2bd795db2b23c0362815c600b189e
delee <maryy1016@gmail.com>
2025-08-21 19:02:36 +0900

    approvalLine finish and signature

--- backend/workflow/src/main/java/com/workflow/repository/ApprovalRepository.java
+++ backend/workflow/src/main/java/com/workflow/repository/ApprovalRepository.java
@@ -1,10 +1,18 @@
 package com.workflow.repository;
 
+import com.workflow.constants.APPROVALSTATUS;
 import com.workflow.entity.ApprovalLine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ApprovalRepository extends JpaRepository<ApprovalLine, Long> {
     List<ApprovalLine> findAllByUserIdAndStatusIsNotNull(Long userId);
+
+    //pending->approved 바꾸기 위해 가져오는
+    Optional<ApprovalLine> findByUserIdAndDocumentIdAndStatus(Long userId, Long DocumentId, APPROVALSTATUS status);
+
+    //다음 결재자 가져옴
+    Optional<ApprovalLine> findByDocumentIdAndApprovalOrder(Long documentId, int approvalOrder);
 }
--- backend/workflow/src/main/java/com/workflow/service/ApprovalService.java
+++ backend/workflow/src/main/java/com/workflow/service/ApprovalService.java
@@ -1,14 +1,20 @@
 package com.workflow.service;
 
 import com.workflow.DTO.request.UserListRequest;
+import com.workflow.constants.APPROVALSTATUS;
+import com.workflow.constants.DOCUMENTSTATUS;
 import com.workflow.constants.ROLE;
+import com.workflow.entity.ApprovalLine;
+import com.workflow.entity.Document;
 import com.workflow.entity.User;
 import com.workflow.repository.ApprovalRepository;
+import com.workflow.repository.DocumentRepository;
 import com.workflow.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +24,7 @@ import java.util.List;
 public class ApprovalService {
     private final ApprovalRepository approvalRepository;
     private final UserRepository userRepository;
+    private final DocumentRepository documentRepository;
 
     //서류 해당 결재자 정보 가져오기
     public List<UserListRequest> getFlowByDoc (Long teamId, Long upperLevel){
@@ -29,6 +36,7 @@ public class ApprovalService {
 
             List<UserListRequest> approvalRequestList = new ArrayList<>();
 
+            //팀장
             UserListRequest req = new UserListRequest();
             req.setUserId(manager.getUserId());
             req.setName(manager.getName());
@@ -36,6 +44,7 @@ public class ApprovalService {
             req.setPositionName(manager.getPosition().getPositionName());
             approvalRequestList.add(req);
 
+            //내 바로 위 사람 있음 가져가기
             if(upper !=null) {
                 UserListRequest req1 = new UserListRequest();
                 req1.setUserId(upper.getUserId());
@@ -54,5 +63,27 @@ public class ApprovalService {
 
     public User getLevel (String email){
         return userRepository.findByEmail(email).orElseThrow(() -> new IllegalArgumentException("레벨 가져오기 실패"));
+    }
+
+    public void approveDocument(Long userId, Long documentId) {
+        Document document = documentRepository.findById(documentId).orElseThrow(() -> new NullPointerException("서류없음"));
+        //가져온 유저아이디랑 서류아이디로 조회 approved로 바꾸고
+        ApprovalLine approvalLine = approvalRepository.findByUserIdAndDocumentIdAndStatus(userId, documentId, APPROVALSTATUS.PENDING)
+                .orElseThrow(() -> new IllegalStateException("유저 서류 pending인 상태 없음"));
+        approvalLine.setStatus(APPROVALSTATUS.APPROVED);
+        approvalLine.setApprovedAt(LocalDateTime.now());
+        approvalRepository.save(approvalLine);
+
+        // 해당서류에서 다음 사람 가져오기.(위에서 가져온 order+1로 조회하고,있으면 다음 단계 없으면 docstatus approved
+        int nextOrder = approvalLine.getApprovalOrder() +1;
+        ApprovalLine nextOne = approvalRepository.findByDocumentIdAndApprovalOrder(documentId, nextOrder)
+                .orElse(null);
+
+        //nextOne이 null이 아니면 상태 바꾸고, 없으면 서류 approved
+        if(nextOne != null){
+            nextOne.setStatus(APPROVALSTATUS.PENDING);
+        }else{
+            document.setStatus(DOCUMENTSTATUS.APPROVED);
+        }
     }
 }
