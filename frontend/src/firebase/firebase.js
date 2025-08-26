e657265a6d535357cad02a7ed7a320b1d2b4206f
daeun <maryy1016@gmail.com>
2025-08-26 18:17:38 +0900

    l

--- frontend/src/firebase/firebase.js
+++ frontend/src/firebase/firebase.js
@@ -9,7 +9,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAixXwZU1G1ioD-HThGBFmTStWZcccfge8",
+  apiKey: "AIzaSyAixXwZU1G1ioD-HThGBFmTStWZcccfge",
   authDomain: "docflow-3d011.firebaseapp.com",
   projectId: "docflow-3d011",
   storageBucket: "docflow-3d011.firebasestorage.app",
