d42ccf0a1cff3d63397338d3e065405680e4a5ef
delee02 <maryy1016@gmail.com>
2025-09-04 18:23:01 +0900

    Update firebase.js

--- frontend/src/firebase/firebase.js
+++ frontend/src/firebase/firebase.js
@@ -9,13 +9,6 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAan8Li9qkkU629uSbud53fAoAjKWglF8s",
-  authDomain: "docflow-2c674.firebaseapp.com",
-  projectId: "docflow-2c674",
-  storageBucket: "docflow-2c674.firebasestorage.app",
-  messagingSenderId: "666587614235",
-  appId: "1:666587614235:web:a24c67619ec4c9695f7cbe",
-  measurementId: "G-7GSKM0N71R"
 };
 
 // Initialize Firebase
